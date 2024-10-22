c4ad62cd652fd24e81ccfa26193e99e40102162d
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-10-23 01:08:18 +0200

    feat: add Navbar layout and pages templates with routing
    This adds the Navbar and its components, sets it as layout visible on all pages and creates routes by setting up templates for each page in the 'app/' directory

---
+++ src/app/user/profile/page.tsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function Profile() {
+    return (
+        <div>
+            <Typography variant="h5">Profile</Typography>
+        </div>
+    );
+};
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import React, { useState } from 'react';
+import Search from '@mui/icons-material/Search';
+import { grey } from '@mui/material/colors';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <form className="flex items-center max-w-sm mx-auto">
+                <label htmlFor="search" className="sr-only">Search</label>
+                <div className="relative w-full">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none focus:ring-green-300">
+                        <Search sx={{ color: grey[700] }}/>
+                    </div>
+                    <input type="text" id="search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                        block w-full ps-10 p-2.5 focus:outline-green-500 " placeholder="Search..." required
+                     />
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SearchBar;
