6b74c0cfbe2a7483527462d7163411f39ee69d23
pablogaraay <garayperezpablo@gmail.com>
2024-10-29 18:17:51 +0100

    feat: add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,29 @@
-
-
 import React from 'react';
 import { Typography } from '@mui/material';
 
 export default function About() {
     return (
         <div>
-            <Typography variant="h5">About</Typography>
+            <div className='flex mx-auto px-32 mt-16'>
+                <Typography variant="h5" className='text-center font-bold'>About</Typography>
+            </div>
+            <div className='flex mx-auto px-32 mt-12 '>
+                 <p className="text-teal-700 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel commodo turpis. Quisque ullamcorper sodales neque, aliquam varius tellus congue sit amet. Etiam id ullamcorper metus. Nullam eget molestie risus. Proin libero est, cursus a mauris lacinia, porttitor scelerisque lectus. Aenean auctor congue malesuada. Aenean dapibus feugiat tellus, nec tincidunt nibh. Nunc semper tincidunt est, a tincidunt massa cursus ac. Donec laoreet porttitor nunc non finibus. Maecenas pellentesque ultricies nisl. Praesent lobortis dapibus lacus non consectetur. Vivamus dapibus nibh quis egestas luctus. Sed ullamcorper id nunc in dapibus. Proin eu suscipit velit. Vivamus imperdiet et est quis ornare. Integer gravida enim dui, id interdum lectus sagittis et. <br/>
+
+                <br/>
+                Morbi orci sapien, sodales vitae dolor scelerisque, finibus rhoncus urna. Praesent tincidunt turpis id est cursus, id viverra quam tristique. Cras elementum facilisis dui, tempus elementum mauris ullamcorper sed. Pellentesque consectetur gravida justo, sit amet volutpat libero maximus in. Fusce in dolor venenatis, fermentum enim pharetra, faucibus turpis. Donec in ultricies quam. Morbi pretium dapibus ornare. Nam porttitor turpis sed sapien posuere, pellentesque ultricies metus tempus. Aliquam erat volutpat. Sed a mauris orci. Integer molestie libero tempus metus fringilla scelerisque.<br/>
+
+                <br/>
+                Mauris nibh lacus, ullamcorper id pretium eget, vehicula malesuada mauris. Cras accumsan libero faucibus, vulputate dolor vitae, faucibus quam. Vestibulum sit amet nunc velit. Donec vitae sapien sed turpis pretium egestas nec eu nibh. Donec elementum libero ac lorem auctor dictum. Nulla tincidunt mattis varius. Nam vestibulum felis quam, vel tincidunt nibh aliquam eget. Mauris convallis egestas nulla, ut interdum mi finibus at. Curabitur non feugiat leo. Suspendisse malesuada imperdiet mi quis dapibus. Sed efficitur mattis sapien, sed venenatis sem hendrerit at. In sed finibus lorem, condimentum tempor arcu.<br/>
+
+                <br/>
+                Suspendisse fermentum nisl a leo rutrum iaculis nec ac magna. Ut sagittis, lorem vel posuere porttitor, lorem sem gravida lacus, eu placerat nisi orci ut leo. Nulla mollis risus nunc, at placerat libero tempus sed. Nulla aliquet eleifend rhoncus. Quisque lorem mi, efficitur ac velit tempor, euismod consequat ante. Cras eros lacus, pretium sed ligula tincidunt, pretium feugiat felis. Morbi lobortis, lacus nec laoreet convallis, lectus felis lobortis augue, ac posuere ligula turpis ut metus. Ut vehicula varius sapien nec elementum. Aenean porttitor vel mi nec consequat.<br/>
+
+                <br/>
+                Sed pulvinar massa in fermentum scelerisque. In hac habitasse platea dictumst. Fusce eleifend felis nec lectus dapibus ullamcorper. Nam suscipit efficitur libero, eu tempor lectus tempus a. Aenean lacinia lacus quis libero aliquet porta. Morbi congue, nisl eget vestibulum semper, est eros dignissim libero, at cursus libero turpis nec mauris. Nulla consequat viverra est, id volutpat nulla dictum sit amet. Nam ornare consectetur nulla et pharetra. Fusce convallis maximus diam, non venenatis felis. Phasellus volutpat dolor sit amet nulla aliquet, sit amet gravida neque ultrices. Mauris feugiat ac magna id ultricies. Fusce ullamcorper iaculis blandit. Etiam faucibus, libero vel venenatis consectetur, ligula risus viverra nunc, vitae vestibulum dui diam in dolor.</p><br/>
+                <br/>
+
+            </div>
         </div>
     );
 };
