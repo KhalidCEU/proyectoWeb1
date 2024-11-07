7c15bab6aaaa9444a6027e476c0d774ab0e25449
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-11-07 21:50:06 +0100

    feat: highlight current page's title on user sidebar

--- src/components/UserSidebar.tsx
+++ src/components/UserSidebar.tsx
@@ -1,19 +1,28 @@
+"use client"
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const UserSidebar = () => {
+  const pathname = usePathname();
+
+  const getLinkClass = (linkPath: string) => {
+    return pathname == linkPath ? 'text-green-600' : 'text-black'
+  }
+
   return (
     <div className="w-44 bg-gray-50 h-screen fixed left-0 top-0 p-4 pt-20">
       <nav>
         <p className="text-2xl font-semibold mb-10">User</p>
         <ul className="flex flex-col items-start gap-6 pl-4">
           <li className="mb-2">
-            <Link href="/user/profile" className="hover:text-green-600">Profile</Link>
+            <Link href="/user/profile" className={getLinkClass('/user/profile')}>Profile</Link>
           </li>
           <li className="mb-2">
-            <Link href="/user/wishlist" className="hover:text-green-600">Wishlist</Link>
+            <Link href="/user/wishlist" className={getLinkClass('/user/wishlist')}>Wishlist</Link>
           </li>
           <li className="mb-2">
-            <Link href="/user/settings" className="hover:text-green-600">Settings</Link>
+            <Link href="/user/settings" className={getLinkClass('/user/settings')}>Settings</Link>
           </li>
         </ul>
       </nav>
