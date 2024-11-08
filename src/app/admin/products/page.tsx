b728dffba3e87288ed22599f78374477a58a3539
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-11-08 16:30:30 +0100

    refactor: move useState and useEffect imports next to React import
    This improves code organization

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import React from "react";
+import { useState, useEffect } from "react";
 import ProductCard from "@/components/ProductCard";
 import testDataFile from "@/testJsons/items.json";
 import SearchBar from "@/components/SearchBar";
-import { useState, useEffect } from "react";
 
 export default function AdminProducts() {
     const testData = testDataFile.items;
