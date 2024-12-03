a16ba835930a5f90e51e09a4b52e2efd5eaf95f1
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-12-03 03:36:46 +0100

    refactor: separate admin products service from regular user service

---
+++ frontend/src/services/products.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import 'dotenv/config';
+
+export function useProductsService() {
+    const url = process.env.NEXT_PUBLIC_API_URL;
+    // console.log("Api url is: ", url);
+
+    const getProducts = async () => {
+        try {
+            const response = await axios.get(`${url}/products`,
+                { withCredentials: true }
+            );
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    const getProductById = async (productId: string ) => {
+        try {
+            const response = await axios.post(`${url}/products/${productId}`,
+                { withCredentials: true }
+            );
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    return { getProducts, getProductById };
+}
