ea4da0babe764de34a9de2ed9bb72fe92c3fd468
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-12-08 21:28:43 +0100

    feat: implement wishlist page
    This makes the wishlist page functional, fetching the items marked as favorite by the user

--- server/router/index.ts
+++ server/router/index.ts
@@ -3,6 +3,7 @@ import authRouter from './auth';
 import productsRouter from './products';
 import userRouter from './user';
 import imagesRouter from './images';
+import wishlistRouter from './wishlist';
 
 const router = express.Router()
 
@@ -10,5 +11,7 @@ router.use('/auth', authRouter);
 router.use('/products', productsRouter);
 router.use('/user', userRouter);
 router.use('/images', imagesRouter);
+router.use('/wishlist', wishlistRouter);
+
 
 export default router;
